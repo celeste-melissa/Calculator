66e4d96e583922c9914166074bdb5a068fddbb42
Celeste <93024079+celeste-melissa@users.noreply.github.com>
2025-07-05 21:26:32 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,11 +5,11 @@ public class Calculator{
 		// Take three parameters - 1 operator, 2 numbers - perform calculation
 		Scanner scanner = new Scanner(System.in); // initialize scanner
 		// First number
-		System.out.print("Enter number: ");
+		System.out.print("Enter first number: ");
 		double firstNum = scanner.nextDouble();
 
 		// Second number 
-		System.out.print("Enter number: ");
+		System.out.print("Enter second number: ");
 		double secondNum = scanner.nextDouble();
 
 		// Operation to peform on first and second numbers
@@ -18,39 +18,39 @@ public class Calculator{
 		char operation = scanner.next().charAt(0);
 
 		// Store the result of the operation
-		Double result;
-		String formatResult;
+		Double result = null;
 
 		// Logic to perform calculation - switch statement will indicate which operation to perform on the following numbers
 		switch(operation){
 		case '+':
 			result = firstNum + secondNum;
-			formatResult = String.format("%.2f", result);
-			System.out.println(formatResult);
+			System.out.println(result);
 			break;
 		case '-':
 			result = firstNum - secondNum;
-			formatResult = String.format("%.2f", result);
-			System.out.println(formatResult);
+			System.out.println(result);
 			break;
 		case '*':
 			result = firstNum * secondNum;
-			formatResult = String.format("%.2f", result);
-			System.out.println(formatResult);
+			System.out.println(result);
 			break;
 		case '/':
+			if(secondNum ==0){
+				System.out.println("Error: Cannot divide by zero.");
+				break;
+			}
 			result = firstNum / secondNum;
-			formatResult = String.format("%.2f", result);
-			System.out.println(formatResult);
+			System.out.println(result);
 			break;
 		case '%':
 			result = firstNum % secondNum;
-			formatResult = String.format("%.2f", result);
-			System.out.println(formatResult);
+			System.out.println(result);
 			break;
 		default:
 			System.out.println("Invalid operation selected. Try again");
 
+		} if(result != null){
+			System.out.printf("%.2f%n", result);
 		}
 	}
 }
